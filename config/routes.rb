143982490d8c723f540b0fc448fc4c1d5dfd5945
rikutoman12 <rikutoo1312@gmail.com>
2021-10-19 03:32:00 +0000

    デイリースクラム

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :customers, only: [:show, :edit, :quit,:out,:update]
+resources :customers, only: [:show, :edit,:update]do
+  collection do
+  	     get 'quit'
+  	     patch 'out'
+
+  	   end
+  	 end
 
 end
