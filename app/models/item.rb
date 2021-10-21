585a4f3ab2b8c28b09a61f268c2efe38f8b85c29
EC2 Default User <ec2-user@ip-172-31-9-6.ec2.internal>
2021-10-21 10:45:01 +0000

    admin items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
     attachment :image
+    belongs_to :genre
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :genre_id, presence: true
+  validates :introduction, presence: true
+  validates :is_active, presence: true
 end
