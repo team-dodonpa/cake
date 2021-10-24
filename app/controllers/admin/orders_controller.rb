140182a71a6675a847de50193d069407b46319af
shinya852 <shinya.june@gmail.com>
2021-10-24 03:47:16 +0000

    create admin order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,19 +1,25 @@
 class Admin::OrdersController < ApplicationController
-    
+
     #before_action :authenticate_admin!
-  
+
   def index
       @orders = Order.all.page(params[:page]).per(10)
   end
-  
+
   def show
       @order = Order.find(params[:id])
       #@order_details = @order.order_details←detail設定後解禁
   end
-  
+
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order)
+    else
+      render "show"
+    end
   end
-  
+
 
 
 end
