bb1467f16af7b1a4d20491512fccbd07ddc2d53e
shinya852 <shinya.june@gmail.com>
2021-10-26 09:01:48 +0000

    index

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -6,5 +6,5 @@ class OrderDetail < ApplicationRecord
 
 
   #enum making_status: {"着手不可":0,"制作待ち":1,"制作中":2,"制作完了":3}
-  enum making_status: {"":0,　"Waiting_for_make":1,  "making":2, "complete",3}
+  enum making_status: {"not_statable":0,"waiting_for_make":1, "making":2,"complete":3}
 end
